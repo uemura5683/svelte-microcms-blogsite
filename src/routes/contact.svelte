529af4908f348e3552f22eb75e2893438d208b40
uemuragame5683 <nobuyoshi5683@gmail.com>
2022-01-26 00:53:04 +0900

    一旦コミット

---
+++ src/routes/contact.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>CONTACT</title>
+</svelte:head>
+
+<h1>CONTACT this site</h1>
+
+<p>CONTACT</p>
---
+++ src/routes/profile.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>Profile</title>
+</svelte:head>
+
+<h1>Profile this site</h1>
+
+<p>Profile</p>
